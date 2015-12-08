d1a8f2b98fcbfbde2e8e119213be1d008c95304d
John <Admin@macbook-pro-de-jonathan.home>
2015-12-08 18:11:53 +0100

    last modif

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -16,15 +16,22 @@ t_list	*ft_lstnew(void const *content, size_t content_size)
 {
 	t_list *new;
 
+	new = NULL;
+	new = (t_list *)malloc(sizeof(t_list));
+	if (!new)
+		return (NULL);
 	if (content == NULL)
 	{
-		new = ft_memalloc(0);
 		new->content = NULL;
+		new->content_size = 0;
 	}
 	else
 	{
-		new = ft_memalloc(content_size);
-		new->content = (void *)content;
+		new->content = malloc(content_size);
+		if (new->content == NULL)
+			return (NULL);
+		ft_memcpy(new->content, content, sizeof(content));
+		new->content_size = content_size;
 	}
 	new->next = NULL;
 	return (new);
