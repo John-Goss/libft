bfc7c95041801f32067c5c4d60c754d0dabb416a
Jonathan LE QUERE <jle-quer@e3r8p9.42.fr>
2016-02-18 12:45:13 +0100

    modif mem fct

--- ft_memalloc.c
+++ ft_memalloc.c
@@ -6,7 +6,7 @@
 /*   By: jle-quer <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/03 11:30:30 by jle-quer          #+#    #+#             */
-/*   Updated: 2015/12/07 12:17:10 by jle-quer         ###   ########.fr       */
+/*   Updated: 2016/02/18 12:44:50 by jle-quer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	*ft_memalloc(size_t size)
 {
 	void	*new;
 
-	new = (void *)malloc(sizeof(size_t) * size);
+	new = (void *)malloc(sizeof(new) * size);
 	if (new == NULL)
 		return (NULL);
 	ft_memset(new, 0, size);
